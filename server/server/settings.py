0a0ff48ef1bc86d7bbfd315d623a3034bfc06c30
jiayishen21 <lucasshen21@gmail.com>
2024-01-18 00:57:43 -0500

    set up docker db config

--- server/server/settings.py
+++ server/server/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'unfollowed',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'db',  # Or use the service name defined in docker-compose
+        'PORT': '6543',
     }
 }
 
