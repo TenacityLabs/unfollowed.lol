2776d64621a9d080fdf346ac1b990e50e309def5
jiayishen21 <lucasshen21@gmail.com>
2024-03-26 21:39:20 -0400

    added basic stuff to chrome extension

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,6 +1,38 @@
+chrome.runtime.onMessage.addListener(
+  function (message, sender, sendResponse) {
+    alert(message)
+    // Do something with the message
+    sendResponse({ farewell: "goodbye" });
+  }
+);
+
 document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('button1').addEventListener('click', function () {
-    alert('clicked')
-    var body = document.getElementsByTagName('body')[0].style.backgroundColor = 'aqua'
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const currentTab = tabs[0];
+    if (currentTab.url && currentTab.url.includes('instagram.com')) {
+      const element = document.getElementById('goto')
+      if (element) {
+        element.remove();
+      }
+    } else {
+      const element = document.getElementById('analysis')
+      if (element) {
+        element.remove();
+      }
+    }
+  });
+
+  document.getElementById('analysis').addEventListener('click', function () {
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const currentTab = tabs[0];
+      if (currentTab.url && currentTab.url.includes('instagram.com')) {
+        alert('The user is on Instagram.');
+        // Perform your logic here
+      } else {
+        alert('The user is not on Instagram.');
+      }
+    });
+
   })
 })
