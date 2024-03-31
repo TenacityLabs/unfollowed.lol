3a567c87463c71c58962007cd8134bfed93ad3fe
edisonqu <edisonquu@gmail.com>
2024-03-31 00:54:11 -0400

    feat: change models so that it will be json field and no relationals

--- server/server/models.py
+++ server/server/models.py
@@ -1,15 +1,14 @@
 from django.db import models
+from django.db.models import JSONField
 
 class User(models.Model):
     username = models.CharField(max_length=100, unique=True)
     insta_name = models.CharField(max_length=100, blank=True, null=True)
-    followers = models.ManyToManyField('self', symmetrical=False, blank=True, related_name='user_followers')
-    following = models.ManyToManyField('self', symmetrical=False, blank=True, related_name='user_following')
-    fans = models.ManyToManyField('self', symmetrical=False, blank=True, related_name='user_fans')
-    unfollowers = models.ManyToManyField('self', symmetrical=False, blank=True,related_name='user_unfollowers')
+    followers = JSONField(default=list, blank=True)
+    following = JSONField(default=list, blank=True)
+    fans = JSONField(default=list, blank=True)
+    unfollowers = JSONField(default=list, blank=True)
     isProcessed = models.BooleanField(default=False)
-
-
 
     def __str__(self):
         return self.username
