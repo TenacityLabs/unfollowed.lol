f7d3dc8ca6b1c41d67edee4445599521ed70a400
edisonqu <edisonquu@gmail.com>
2024-03-30 23:36:19 -0400

    feat: bulk creation

--- server/api/views.py
+++ server/api/views.py
@@ -4,6 +4,7 @@ from server.serializers import UserSerializer
 from server.models import User
 from django.http import JsonResponse
 import json
+from django.db import transaction
 
 
 @api_view(['GET'])
@@ -18,24 +19,31 @@ def receiveData(request):
     username = data.get('username')
     user, created = User.objects.get_or_create(username=username)
 
-    for field in ['followers', 'followings', 'unfollowers', 'fans']:
-        related_users = data.get(field, [])
-        for related_user_data in related_users:
-            related_user, _ = User.objects.get_or_create(
-                username=related_user_data['username'],
-                defaults={'insta_name': related_user_data.get('insta_name', '')}
-            )
-            
+    with transaction.atomic():
+        for field in ['followers', 'followings', 'unfollowers', 'fans']:
+            related_users = data.get(field, [])
+            related_user_instances = []
+            for related_user_data in related_users:
+                related_user_instance, _ = User.objects.update_or_create(
+                    username=related_user_data['username'],
+                    defaults={'insta_name': related_user_data.get('insta_name', '')}
+                )
+                related_user_instances.append(related_user_instance)
+
             if field == 'followers':
-                user.followers.add(related_user)
+                user.followers.add(*related_user_instances)
             elif field == 'followings':
-                user.following.add(related_user)
+                user.following.add(*related_user_instances)
             elif field == 'unfollowers':
-                user.unfollowers.add(related_user)
+                user.unfollowers.add(*related_user_instances)
             elif field == 'fans':
-                user.fans.add(related_user)
-                
+                user.fans.add(*related_user_instances)
+
+    user.isProcessed = True
+    user.save()
+
     return Response({'status': 'success'})
+
 
 @api_view(['GET'])
 def userProfile(request, username):
