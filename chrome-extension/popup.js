d3b9fc404203f2c60e9e2ccd6da8d574b349ffef
Andrew Durnford <andrewhdurnford@gmail.com>
2024-04-02 19:52:52 -0400

    added server integration and analysis preview to extension window

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -2,6 +2,29 @@ document.addEventListener('DOMContentLoaded', function () {
   // const text = document.querySelector('.loader-text p')
   // text.innerHTML = text.innerText.split("").map((char, i) => `<b style="transform:rotate(${i * 6.8}deg)">${char}</b>`).join("")
 
+  let username;
+
+  chrome.cookies.get({ url: 'https://www.instagram.com', name: 'ds_user_id' }, function(cookie) {
+    if (cookie) {
+        const ds_user_id = cookie.value;
+        fetch(`https://www.instagram.com/graphql/query/?query_hash=c9100bf9110dd6361671f113dd02e7d6&variables=%7B%22user_id%22:%22${ds_user_id}%22,%22include_chaining%22:false,%22include_reel%22:true,%22include_suggested_users%22:false,%22include_logged_out_extras%22:false,%22include_highlight_reels%22:false,%22include_related_profiles%22:false%7D`)
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            return response.json();
+          })
+          .then(data => {
+            username = data.data.user.reel.user.username;
+          })
+          .catch(error => {
+            console.error('Error fetching data:', error);
+          });
+    } else {
+        console.log('User not logged in.');
+    }
+  });
+
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     const currentTab = tabs[0];
 
@@ -18,44 +41,48 @@ document.addEventListener('DOMContentLoaded', function () {
       document.getElementById('analysis').className = 'hidden'
       loading = document.getElementById('loading').className = 'action'
 
-      chrome.scripting.executeScript({
-        target: { tabId: currentTab.id },
-        function: async () => {
-          const profileElements = document.querySelectorAll('.x1i10hfl.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz._a6hd');
-          for (const element of profileElements) {
-            if (element.href && element.textContent.trim() === 'Profile') {
-              const username = element.href.match(/https:\/\/www\.instagram\.com\/([^\/]+)\//)[1]
-              return username
-            }
-          }
-          return { err: 'Username not found' }
-        }
-      }, (res) => {
-        if (res[0].result.err) {
-          alert('An error as occured, please try again')
-          return
-        }
-        const username = res[0].result
+      // Send the username to the background script
+  chrome.runtime.sendMessage({ username: username }, async response => {
+    const postData = { ...response, username: username };
+    console.log(postData)
 
-        chrome.runtime.sendMessage({ username: username }, response => {
-          if (!response || !response.followers || !response.followings || !response.unfollowers || !response.fans) {
-            alert('An error as occured, please try again')
-            return
-          }
-          const { followers, followings, unfollowers, fans } = response
-          console.log({ followers, followings, unfollowers, fans })
+    try {
+      // TODO: Update this to put into .env or remember to switch during production
+      // TODO: Use django api
 
-          // FIXME, remove the temp
-          // chrome.storage.local.set({ username }, function () {
-          chrome.storage.local.set({ username: "TEMP", followers, followings, unfollowers, fans }, function () {
-            document.documentElement.className = 'profile-html'
-            document.body.className = 'profile-body'
-            document.getElementById('nouser').className = 'hidden'
-            document.getElementById('profile').className = ''
-          })
-        });
-      })
-    })
+      const apiUrl = 'http://127.0.0.1:8000/receive';
+      const fetchResponse = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(postData),
+      });
+
+      if (!fetchResponse.ok) {
+        throw new Error(`HTTP error! status: ${fetchResponse.status}`);
+      }
+
+      const data = await fetchResponse.json();
+      
+      console.log('Response from Django API:', data);
+      
+      document.getElementById('unfollowers').innerHTML = (Math.round(postData.unfollowers.length / postData.followings.length * 100) || 0) + '%'
+      document.getElementById('fans').innerHTML = (Math.round(postData.fans.length / postData.followers.length * 100) || 0) + '%'
+      document.documentElement.className = 'profile-html'
+      document.body.className = 'profile-body'
+      document.getElementById('nouser').className = 'hidden'
+      document.getElementById('profile').className = 'profile'
+      document.getElementById('link').addEventListener('click', function() {
+        window.open(`https://www.unfollowed.lol/user/${username}/`, '_blank'); 
+      });
+
+    } catch (error) {
+      alert(`Error fetching: ${username}`)
+      console.error('Error posting data to Django:', error);
+    }
+  });
+  })
 
     if (currentTab.url && currentTab.url.includes('instagram.com')) {
       // remove goto element from the popup
