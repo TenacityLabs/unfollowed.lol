cacdd5c704f73034683d76fca3de903f641dc2b7
Andrew Durnford <andrewhdurnford@gmail.com>
2024-04-06 03:23:02 -0400

    change user profile api response

--- server/server/serializers.py
+++ server/server/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from .models import User
 from .models import Transaction
+import humanize
+from django.utils import timezone
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,9 +15,22 @@ class UserProfileSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'insta_name', 'followers', 'following', 'fans', 'unfollowers']
 
+
 class TransactionSerializer(serializers.ModelSerializer):
     to_user = UserSerializer()
 
     class Meta:
         model = Transaction
-        fields = ['from_user', 'to_user', 'action', 'timestamp']
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        timestamp = instance.timestamp
+        
+        # Assuming 'timestamp' is a datetime object and using Django's timezone support
+        now = timezone.now()
+        if timestamp:
+            delta = now - timestamp
+            human_readable = humanize.naturaltime(delta)
+            ret['timestamp'] = human_readable
+        return ret
