5b580065f309280f81e3bf4a81df609d037e8cbb
edisonqu <edisonquu@gmail.com>
2024-07-07 14:40:28 -0400

    remove

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import getData, receiveData, userProfile, getRecentTransactions, getAllTransactions, serve_verification_file
+from .views import getData, receiveData, userProfile, getRecentTransactions, getAllTransactions
 
 urlpatterns = [
     path('', getData),
@@ -8,6 +8,5 @@ urlpatterns = [
     path('user/<str:username>/', userProfile),
     path('transactions/<str:username>/', getRecentTransactions),
     path('transactions/<str:username>/all/', getAllTransactions),
-    path('.well-known/pki-validation/B45539896C49210367274D3AD1C60BD5.txt', serve_verification_file),
 
 ]
