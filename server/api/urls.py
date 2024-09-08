e703294ebedfcd8c1b541b1c5ca113029a19193f
edisonqu <edisonquu@gmail.com>
2024-09-07 22:40:27 -0400

    remove transactions

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import getData, receiveData, userProfile #, getRecentTransactions, getAllTransactions
+from .views import getData, receiveData, userProfile
 
 urlpatterns = [
     path('', getData),
--- server/api/views.py
+++ server/api/views.py
@@ -2,8 +2,6 @@ import datetime
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from server.serializers import TransactionSerializer, UserProfileSerializer, UserSerializer
-from server.models import User, Transaction
 from django.http import JsonResponse
 import json
 from django.db import transaction
