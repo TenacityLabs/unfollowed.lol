45eea740e471b48eb17e683848efd66c27a035d7
edisonqu <edisonquu@gmail.com>
2024-10-13 01:41:45 -0400

    remove maximums

--- server/server/models.py
+++ server/server/models.py
@@ -3,14 +3,14 @@ from django.db.models import JSONField
 
 class User(models.Model):
     # user data
-    username = models.CharField(max_length=100, unique=True, default='')
-    insta_name = models.CharField(max_length=100, blank=True, null=True)
-    avatar_url = models.URLField(max_length=400, blank=True)
+    username = models.CharField( unique=True, default='')
+    insta_name = models.CharField( blank=True, null=True)
+    avatar_url = models.URLField( blank=True)
 
     # interaction data
-    username = models.CharField(max_length=100, default='', blank=False, null=False)
-    insta_name = models.CharField(max_length=100, blank=True, null=True)
-    avatar_url = models.URLField(max_length=400, blank=True, null=True)
+    username = models.CharField( default='', blank=False, null=False)
+    insta_name = models.CharField( blank=True, null=True)
+    avatar_url = models.URLField(blank=True, null=True)
     followers = JSONField(default=list, blank=True)
     following = JSONField(default=list, blank=True)
     fans = JSONField(default=list, blank=True)
